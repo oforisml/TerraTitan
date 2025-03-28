cffac5c770f1765467bc6a148c1cba09f7ce36bc
Charles Martinot <charles.martinot@protonmail.com>
2025-03-28 10:56:42 +0800

    Re-add weather workflow

--- data/scripts/mastra-workflow/src/mastra/index.ts
+++ data/scripts/mastra-workflow/src/mastra/index.ts
@@ -4,12 +4,14 @@ import { Mastra } from "@mastra/core";
 import { candidateWorkflow } from "./workflows/candidate";
 import { myWorkflow } from "./workflows/demo";
 import { convertWorkflow } from "./workflows/convert";
+import { weatherWorkflow } from "./workflows/weather";
 
 export const mastra = new Mastra({
   // agents: { weatherAgent },
   workflows: {
     candidateWorkflow,
     myWorkflow,
+    weatherWorkflow,
     convertWorkflow,
   },
 });
